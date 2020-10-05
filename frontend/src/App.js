3e00c61dff1a2a7207ce4481831decefda0b6c30
Thomas Ree Hjelm <github@thomasnotes.com>
2020-10-05 23:40:46 +0200

    Added Routing and single Product Screen

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,21 +1,23 @@
 import React from 'react'
-import { BrowserRouter as Router } from 'react-router-dom'
 import { Container } from 'react-bootstrap'
 import Header from './components/Header'
 import Footer from './components/Footer'
 import HomeScreen from './screens/HomeScreen'
+import ProductScreen from './screens/ProductScreen'
+import { BrowserRouter, Route } from 'react-router-dom'
 
 const App = () => {
   return (
-    <>
+    <BrowserRouter>
       <Header />
       <main className='py-3'>
         <Container>
-          <HomeScreen />
+          <Route path='/' component={HomeScreen} exact />
+          <Route path='/product/:id' component={ProductScreen} />
         </Container>
       </main>
       <Footer />
-    </>
+    </BrowserRouter>
   )
 }
 
